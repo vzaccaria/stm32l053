2b2540b0b125ae6699c36dd5d5d1791fb1b9f20d
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-09-13 19:21:32 +0200

    feat: send data through the protobuf

--- example2/src/main.cpp
+++ example2/src/main.cpp
@@ -3,18 +3,38 @@
 /* Needed by RTOS when syntax checking through Emacs */
 #define TOOLCHAIN_GCC
 #include "rtos.h"
+#include "proto/message.pb-c.h"
+
+/* We statically allocate the buffer in which to pack the message */
+#define PCT_BUF_SIZE 128
+
+Serial pc(SERIAL_TX, SERIAL_RX);
 
 DigitalOut led1(LED2);
 
 int main() {
 
     Thread thread([](void const *){
+            uint8_t buf[PCT_BUF_SIZE];
+
+            auto sendData = [&](const Pct *m) {
+                auto len = pct__get_packed_size(m);
+                pct__pack(m, buf);
+                for(uint i=0; i<len; i++) {
+                    pc.printf("%d", buf[i]);
+                }
+            };
+
             while (true) {
                 led1 = !led1;
+                Pct m = PCT__INIT;
+                m.pressure = 0.3f;
+                m.temp = 52.0f;
+                m.humidity = 10.0f;
+                sendData(&m);
                 Thread::wait(200);
             }
         });
-
 
     while(true) {
     }
